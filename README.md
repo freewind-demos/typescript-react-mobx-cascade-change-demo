d9a803491eaa5cd12fce6848c93fd8afcbc7c052
Freewind <nowindlee@gmail.com>
2021-02-21 02:23:50 +0800

    update observe to reaction

--- README.md
+++ README.md
@@ -3,6 +3,9 @@ TypeScript React MobX Cascade Change Demo
 
 https://stackoverflow.com/questions/50746872/mobx-how-to-change-an-observable-value-in-response-to-another-values-change
 
+应该使用`reaction`，因为`observe`是底层函数，我们应该尽量避免使用：
+https://mobx.js.org/intercept-and-observe.html
+
 ```
 npm install
 npm run demo
--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,10 @@
-import {observable, observe} from 'mobx';
+import {observable, reaction} from 'mobx';
 
 export class Store {
 
   constructor() {
-    observe(this, 'selectedValue1', () => {
+    // observe(this, 'selectedValue1', () => {
+    reaction(() => this.selectedValue1, () => {
       this.selectedValue2 = '';
     })
   }
